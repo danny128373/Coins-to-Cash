36f3c6940b7c19bc5e06c831c5dba83be0f151ee
Daniel Meza <daniel.meza29@yahoo.com>
2020-04-27 14:18:14 -0500

    Initial commit

---
+++ coinsToCash.js
@@ -0,0 +1,13 @@
+const piggyBank = {
+  pennies: 150,
+  nickels: 25,
+  dimes: 30,
+  quarters: 40
+}
+
+const dollarAmount = (piggyBank) => {
+  const total = piggyBank.pennies * .01 + piggyBank.nickels * .05 + piggyBank.dimes * .1 + piggyBank.quarters * .25;
+  console.log(`Your total dollar amount is ${total}`);
+}
+
+dollarAmount();
